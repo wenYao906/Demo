03e8c4ad0c1e8796d2f500b5d2ab5abbf2151960
sun yao <316625387@qq.com>
2019-04-10 10:45:36 +0800

    模仿 Soul App

---
+++ 02-SoulDemo/MoMoDemo/MoMoDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoMoDemo
+//
+//  Created by study on 2019/4/10.
+//  Copyright © 2019年 WY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ 02-SoulDemo/SoulDemo/SoulDemoTests/SoulDemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  SoulDemoTests.swift
+//  SoulDemoTests
+//
+//  Created by study on 2019/4/10.
+//  Copyright © 2019年 WY. All rights reserved.
+//
+
+import XCTest
+@testable import SoulDemo
+
+class SoulDemoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
