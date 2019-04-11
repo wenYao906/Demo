9ca7aab9fb82113d2505a4158e4e6684f615485d
sun yao <316625387@qq.com>
2019-04-11 08:23:41 +0800

    内容提交

---
+++ 02-SoulDemo/SoulDemo/Classes/Login/Controller/SLoginController.swift
@@ -0,0 +1,25 @@
+//
+//  SLoginController.swift
+//  SoulDemo
+//
+//  Created by study on 2019/4/10.
+//  Copyright © 2019年 WY. All rights reserved.
+//
+
+import UIKit
+
+class SLoginController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+}
+
+// MARK:- UIView
+extension SLoginController {
+    
+}
---
+++ 02-SoulDemo/SoulDemo/Classes/Login/View/SLoginView.swift
@@ -0,0 +1,30 @@
+//
+//  SLoginView.swift
+//  SoulDemo
+//
+//  Created by study on 2019/4/10.
+//  Copyright © 2019年 WY. All rights reserved.
+//
+
+import UIKit
+
+class SLoginView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension SLoginView {
+    // 上
+    
+    // 中
+    
+    
+    // 下
+}
