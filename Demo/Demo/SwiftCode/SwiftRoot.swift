d542a3eefe5e968d7af65385f7bbcb70eef9b63d
wenYao906 <363536404@qq.com>
2023-04-10 11:00:33 +0800

    1. storyBoard有多个卡片时，创建一个按钮数组，来跟SB上的卡片连接。用index.of来获得当前按钮的下标位置。在用

--- Demo/Demo/SwiftCode/SwiftRoot.swift
+++ Demo/Demo/SwiftCode/SwiftRoot.swift
@@ -12,8 +12,18 @@ class SwiftRoot: UIViewController {
     /// 1.1.4 创建卡片记录的Label
     @IBOutlet weak var flipCountLabel: UILabel!
     
+    @IBOutlet var cardsBtn: [UIButton]!
+    
+    var emojiChoices = ["🎃","😈","🎃","😈"]
+    
+    
     /// 1.1.5 创建一个变量，用来记录卡片的点击次数
-    var flipCount: Int = 0
+    var flipCount = 0 {
+        didSet {
+            // 1.1.7 给记录卡片的Label 添加数据
+            flipCountLabel.text = "Flips:\(flipCount)"
+        }
+    }
     
     /// 1.1.6 创建一个变量，用来保存所有的卡片
     override func viewDidLoad() {
@@ -24,8 +34,15 @@ class SwiftRoot: UIViewController {
     /// 1.1.1 在SB中创建一个UIButton，连线到 ViewController 类中
     /// - Parameter sender: 点击的卡片
     @IBAction func touchCard(_ sender: UIButton) {
-        // 1.1.3 调用卡片翻转方法
-        flipCard(withEmoji: "👻", on: sender)
+        flipCount += 1
+        
+        // 获取点击按钮的索引
+        if let cardNumber = cardsBtn.firstIndex(of: sender) {
+            // 1.1.3 调用卡片翻转方法
+            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+        }else{
+            print("没有获取到点击按钮的索引")
+        }
     }
     
     
@@ -34,12 +51,6 @@ class SwiftRoot: UIViewController {
     ///   - emoji: emoji 图片
     ///   - button: 按钮
     func flipCard(withEmoji emoji: String, on button: UIButton){
-        
-        // 1.1.6 记录卡片的点击次数
-        flipCount += 1
-        // 1.1.7 给记录卡片的Label 添加数据
-        flipCountLabel.text = "Flips:\(flipCount)"
-        
         // 1.1.2.1 让卡片翻转
         if button.currentTitle == emoji {
             button.setTitle("", for: UIControl.State.normal)
